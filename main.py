ad16fec4569e2ffa4bbfca68956d7c0eb971f7cb
1akay007 <148199716+1akay007@users.noreply.github.com>
2023-10-20 22:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
 import  streamlit as st
 
 st.title("welcome to aktech")
-st.header("latest ptice drop on tech products")
-st.subheader()
+import  streamlit as st
+
+st.title("welcome to aktech")
+st.header("latest iphones")
+import streamlit as st
+
+iphones = [
+    {
+        "model": "iPhone 13 Pro",
+        "display": "6.1-inch Super Retina XDR display",
+        "camera": "Triple 12MP camera system",
+        "chip": "A15 Bionic chip",
+        "price": "$999",
+    },
+    {
+        "model": "iPhone 13",
+        "display": "6.1-inch Super Retina XDR display",
+        "camera": "Dual 12MP camera system",
+        "chip": "A15 Bionic chip",
+        "price": "$799",
+    },
+    {
+        "model": "iPhone 13 mini",
+        "display": "5.4-inch Super Retina XDR display",
+        "camera": "Dual 12MP camera system",
+        "chip": "A15 Bionic chip",
+        "price": "$699",
+    },
+]
+
+
+        st.write(f"Camera: {iphone['camera']}")
+        st.write(f"Chip: {iphone['chip']}")
+        st.write(f"Price: {iphone['price']}")# Streamlit web app
+st.title("New iPhones")
+
+# Display a list of iPhone models
+selected_model = st.selectbox("Select an iPhone model", [iphone['model'] for iphone in iphones])
+
+# Display the specifications for the selected iPhone
+for iphone in iphones:
+    if iphone['model'] == selected_model:
+        st.header(iphone['model'])
+        st.write(f"Display: {iphone['display']}")
